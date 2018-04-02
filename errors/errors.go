a5f0cda66f95cef85896cd9ad387d06daae89560
Clark Bynum <cbynum@clearblade.com>
2018-04-02 17:11:08 -0500

    now parsing error info from map

--- errors/errors.go
+++ errors/errors.go
@@ -2,6 +2,7 @@ package errors
 
 import (
 	"encoding/json"
+	"fmt"
 
 	cc "github.com/clearblade/go-utils/errors/constants/category"
 	uuid "github.com/clearblade/go-utils/uuid"
@@ -9,8 +10,8 @@ import (
 
 type Info struct {
 	Id            uuid.Uuid           `json:"id"`
-	Code          int                 `json:"code"`
-	Level         int8                `json:"level"`
+	Code          float64             `json:"code"`
+	Level         float64             `json:"level"`
 	Category      cc.CategoryConstant `json:"category"`
 	Message       string              `json:"message"`
 	Detail        string              `json:"detail"`
@@ -19,8 +20,8 @@ type Info struct {
 }
 
 type Response struct {
-	Info       Info `json:"error"`
-	StatusCode int  `json:"statusCode"`
+	Info       Info    `json:"error" mapstructure:"error"`
+	StatusCode float64 `json:"statusCode"`
 }
 
 func (e *Response) Error() string {
@@ -28,19 +29,58 @@ func (e *Response) Error() string {
 	return string(marsha)
 }
 
-func (e *Response) HTTPStatusCode() int {
+func (e *Response) HTTPStatusCode() float64 {
 	return e.StatusCode
 }
 
 func (e *Response) FromNet(body interface{}) *Response {
-	var errorMap Response
-	err := json.Unmarshal(body.([]byte), &errorMap)
-	if err != nil {
-		return nil
+	var imAMap map[string]interface{}
+	var ok bool
+	if imAMap, ok = body.(map[string]interface{}); !ok {
+		return createEmptyErrorResponse(body)
 	}
-	e.Info = errorMap.Info
-	e.StatusCode = errorMap.StatusCode
-	return e
+
+	var errorInfo map[string]interface{}
+
+	if errorInfo, ok = imAMap["error"].(map[string]interface{}); !ok {
+		return createEmptyErrorResponse(body)
+	}
+	var statusCode float64
+	if statusCode, ok = imAMap["statusCode"].(float64); !ok {
+		return createEmptyErrorResponse(body)
+	}
+
+	// alright, it must be the format we're expecting. let's grab the fields we need
+
+	parsedId, err := uuid.ParseUuid(errorInfo["id"].(string))
+	if err != nil {
+		return createEmptyErrorResponse(body)
+	}
+	info := Info{
+		Id:            parsedId,
+		Message:       errorInfo["message"].(string),
+		Code:          errorInfo["code"].(float64),
+		Level:         errorInfo["level"].(float64),
+		Category:      cc.CategoryConstant(errorInfo["category"].(string)),
+		Detail:        errorInfo["detail"].(string),
+		LowLevelError: fmt.Errorf("%+v", errorInfo["lowLevelError"]),
+		Line:          errorInfo["line"].(string),
+	}
+
+	return &Response{
+		Info:       info,
+		StatusCode: statusCode,
+	}
+}
+
+func createEmptyErrorResponse(body interface{}) *Response {
+	info := Info{
+		Message: fmt.Sprintf("%+v", body),
+	}
+	return &Response{
+		Info:       info,
+		StatusCode: 0,
+	}
 }
 
 func CreateResponseFromNet(body interface{}) *Response {
